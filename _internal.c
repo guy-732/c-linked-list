ab9dd5f750ff11625ed106ce42671a0e59c6d049
guy-732 <81652178+guy-732@users.noreply.github.com>
2022-01-15 23:19:37 +0100

    Supprise tool that will help us later

--- _internal.c
+++ _internal.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "_internal.h"
@@ -12,4 +13,20 @@ ll_node_t * _ll_new_node(ll_value_t val, ll_node_t * prev, ll_node_t * next)
 	res->prev = prev;
 	res->next = next;
 	return res;
+}
+
+void _ll_swap_nodes(ll_node_t * n1, ll_node_t * n2)
+{
+	if (n1 == NULL || n2 == NULL)
+	{
+		#ifndef DEBUG
+		fprintf(stderr, "WARNING FILE %s LINE %d: _ll_swap_nodes received NULL pointer\n",
+				__FILE__, __LINE__);
+		#endif
+		return;
+	}
+
+	ll_value_t tmp = n1->value;
+	n1->value = n2->value;
+	n2->value = tmp;
 }
--- _internal.h
+++ _internal.h
@@ -8,4 +8,6 @@
 ll_node_t * _ll_new_node(ll_value_t val, ll_node_t * prev, ll_node_t * next);
 #define _ll_free_node(node) (free((node)))
 
+void _ll_swap_nodes(ll_node_t * n1, ll_node_t * n2);
+
 #endif
