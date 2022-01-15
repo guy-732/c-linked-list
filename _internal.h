c6a71ca12b1e8c01a888ceb223e80f42ef830e46
guy-732 <81652178+guy-732@users.noreply.github.com>
2022-01-15 22:54:39 +0100

    More parenthesis on macro extension

--- _internal.h
+++ _internal.h
@@ -6,6 +6,6 @@
 #include "linked-list.h"
 
 ll_node_t * _ll_new_node(ll_value_t val, ll_node_t * prev, ll_node_t * next);
-#define _ll_free_node(node) (free(node))
+#define _ll_free_node(node) (free((node)))
 
 #endif
