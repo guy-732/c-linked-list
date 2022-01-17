00b3e285f6347ef7e29fa88f2a8bd5df3dfdcab7
guy-732 <81652178+guy-732@users.noreply.github.com>
2022-01-17 22:18:12 +0100

    Just... a space removed

--- ll-extra.h
+++ ll-extra.h
@@ -6,7 +6,7 @@
 /*
  * Insert a value in sorted order
  * (ll->cmp(val, previous_val) <= 0 && ll->cmp(val, next_val) >= 0)
- * 
+ *
  * Assumes the linked list is already sorted
  */
 bool ll_insert_sorted(linked_list_t * ll, ll_value_t val);
