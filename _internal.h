a7c398c6a3ffba77e175a6cf78c905e27e0e5ac2
guy-732 <81652178+guy-732@users.noreply.github.com>
2022-01-17 22:26:53 +0100

    just ifdef __cplusplus I forgot

--- _internal.h
+++ _internal.h
@@ -7,10 +7,19 @@
 
 #include "linked-list.h"
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 ll_node_t * _ll_new_node(ll_value_t val, ll_node_t * prev, ll_node_t * next);
 #define _ll_free_node(node) (free((node)))
 
 void _ll_swap_nodes(ll_node_t * n1, ll_node_t * n2);
 ll_node_t * _ll_get_node_n(ll_node_t * node, int64_t n, bool forward);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
--- ll-extra.h
+++ ll-extra.h
@@ -3,6 +3,11 @@
 
 #include "linked-list.h"
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 /*
  * Insert a value in sorted order
  * (ll->cmp(val, previous_val) <= 0 && ll->cmp(val, next_val) >= 0)
@@ -10,5 +15,9 @@
  * Assumes the linked list is already sorted
  */
 bool ll_insert_sorted(linked_list_t * ll, ll_value_t val);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
