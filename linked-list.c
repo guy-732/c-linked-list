302e725c9ea07fde09dd4eb96cd0004ccbf2f1a4
guy-732 <81652178+guy-732@users.noreply.github.com>
2022-01-26 23:41:44 +0100

    Optimized internal access

--- linked-list.c
+++ linked-list.c
@@ -54,6 +54,9 @@ bool ll_get_item(const linked_list_t * ll, int64_t index, ll_value_t * res)
 	}
 
 	const ll_node_t * n;
+	if (!_ll_shorter_index(ll_len(ll), &index))
+		return false;
+
 	if (index < 0)
 	{
 		++index;
@@ -81,6 +84,9 @@ bool ll_set_item(linked_list_t * ll, int64_t index, ll_value_t val)
 	}
 
 	ll_node_t * n;
+	if (!_ll_shorter_index(ll_len(ll), &index))
+		return false;
+
 	if (index < 0)
 	{
 		++index;
@@ -108,6 +114,9 @@ bool ll_remove_item(linked_list_t * ll, int64_t index, ll_value_t * res)
 	}
 
 	ll_node_t * n;
+	if (!_ll_shorter_index(ll_len(ll), &index))
+		return false;
+
 	if (index < 0)
 	{
 		++index;
